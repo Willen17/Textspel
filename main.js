7b05c231f2e652e712cc74571d9dd796860e933c
William Saar <williamsaar@outlook.com>
2021-11-15 18:26:48 +0100

    Fixed functionality on the start button

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 // Anropar main funktionen när sidan laddats in
-// window.onload = main; 
+ window.onload = main; 
 
 function main () {
-    addEventListeners()
+    addEventListeners();
 }
 
 // Hämtar element ur HTML:en
 function addEventListeners() {
+    const startButton = document.getElementById('startButton');
+    startButton.onclick = startGame;
 
+    const storyBox = document.getElementById('storyBox');
+
+    const inputBox = document.getElementById('inputBox');
+
+    const choicesBox = document.getElementById('choicesBox');
+
+    const inventory = document.getElementById('inventory');
+}
+
+
+function startGame() {
+   startButton.remove();
+   storyBox.style.display = 'unset';
+   inputBox.style.display = 'unset';
 }
 
 document.getElementById('myButton').onclick = function() {
