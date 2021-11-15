d4c2bec8b56ade2aa795c73f7e8f5e69827a94ed
William Saar <williamsaar@outlook.com>
2021-11-15 10:54:30 +0100

    "Event box almost finished"

---
+++ main.js
@@ -0,0 +1,11 @@
+// Anropar main funktionen när sidan laddats in
+window.onload = main; 
+
+function main () {
+    addEventListeners()
+}
+
+// Hämtar element ur HTML:en
+function addEventListeners() {
+
+}
